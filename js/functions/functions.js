e2d4e9de3c7eae6e96a5b85eca74292b5304c579
Cassio Reis <creisbarbosa@gmail.com>
2022-09-06 07:45:50 -0300

    Argument class

---
+++ js/functions/functions.js
@@ -0,0 +1,14 @@
+// create a motivational quotes app
+// function statement
+function createQuotes(){
+    console.log("Just one small positive thought in the morning can change your whole day.")
+    console.log("Opportunities don't happen, you create them.")
+    console.log("Love your family, work super hard, live your passion.")
+}
+
+// execute, run, call, invoke
+createQuotes()
+createQuotes()
+createQuotes()
+
+console.log(`The End`)
