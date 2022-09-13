7f96e79884fa82654818c97cc3a845cad63dad2b
Cassio Reis <creisbarbosa@gmail.com>
2022-09-13 18:35:00 -0300

    End of javascript course

---
+++ js/07-exercices/05-imc-calculator.js
@@ -0,0 +1,14 @@
+/* BMI Body Mass Indsex Calculator
+
+BMI = Mkg / Hm**2
+
+Calassification
+    * IMC <  16                      -   Magreza grave
+    * IMC >= 16     && IMC <17       -   Magreza moderada
+    * IMC >= 17     && IMC <18.5     -   Magreza leve
+    * IMC >= 18.5   && IMC <25       -   Saudável
+    * IMC >= 25     && IMC <30       -   Sobrepeso
+    * IMC >= 30     && IMC <35       -   Obesidade Grau I
+    * IMC >= 35     && IMC <40       -   Obesidade Grau II
+    * IMC >= 45                      -   Obesidade Grau III
+*/
