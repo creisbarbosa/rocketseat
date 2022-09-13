b45c6cc714d10aeb9a6a2213930963628bacd308
Cassio Reis <creisbarbosa@gmail.com>
2022-09-13 14:15:54 -0300

    Delete

--- js/introduction/comentarios.js
+++
@@ -1,4 +0,0 @@
-// Aqui eu tenho um comentário em linha
-// O código abaixo escreve alguma mesagem no console
-console.log('Bem vindos ao Starter') // Ele ignora esses comentários aqui
-console.log("Não vou ignorar")
