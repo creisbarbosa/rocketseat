c30e4cde0ad3e76490f8d9ca957320f3e21f193b
Cassio Reis <creisbarbosa@gmail.com>
2022-09-22 21:30:03 -0300

    Fist DOM classes

---
+++ dom/DOM.md
@@ -0,0 +1,17 @@
+ # DOM (Document Object Model)
+
+ * É o HTML convertido para um Objeto JavaScript
+ * API que representa e interage com o HTML
+ * Estrutura de dados tipo árvore, criada pelo browser
+ * Propriedades e métodos
+
+ # Para que?
+
+ * JavaScript usa o DOM para se comunicar com o HTML
+ * Manipular o HTML com o JavaScript
+ * Você só programa para a web porque existe a DOM
+
+ * Exemplo
+
+ * Tipo de estrutura de árvos com nós, pais e filhos
+ * document(html(head(meta,meta,title), body(title)))
---
+++ dom/script.js
@@ -0,0 +1,36 @@
+// SELECIONANDO ELEMENTOS
+
+// 01
+// getElementById(element)
+
+// const element = document.getElementById('blog-title')
+// console.log(element)
+
+// 02
+// getElementsByClassName(HTML colection)
+
+// const element = document.getElementsByClassName('one')
+// console.log(element)
+
+// 03
+// getElementsByTagName(HTML colection)
+ 
+// const tag = document.getElementsByTagName('h1')
+// console.log(tag)
+
+// 04
+// querySelector(element)
+
+// const query = documetn.querySelector('')
+
+// console.log(query) // aqui ele só pega o primeriro que ele achar (existe uma maneira de pegar mais do que uma query)
+
+// 05
+/* querySelectorAll(Nodelist) - consigo fazer um forEach
+
+const newQuery = document.querySelectorAll('#none')
+console.log(newQuery)
+
+newQuery.forEach(el => console.log(el)) */
+
+// MANIPULANDO CONTÜDO
